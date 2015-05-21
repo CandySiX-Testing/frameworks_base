80f31950ef12e3d7a400e1433dc46f068242824e
Daniel Koman <dankoman30@gmail.com>
2015-05-20 23:38:10 -0400

    SystemUI: IME key shouldn't cause navbar icons to re-align
    This sets the right-side menu icon's visibility to GONE rather
            than INVISIBLE if the IME button is showing, so that
            the navbar icons don't get pushed over.
    
    Change-Id: I646542f9dc25a672d1413035aa7831de05bdf030

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -99,6 +99,7 @@ public class NavigationBarView extends LinearLayout {
     private int mMenuVisibility;
     private int mMenuSetting;
     private boolean mOverrideMenuKeys;
+    private boolean mIsImeButtonVisible = false;
 
     final Display mDisplay;
     View mCurrentView = null;
@@ -603,6 +604,7 @@ public class NavigationBarView extends LinearLayout {
         final boolean showImeButton = ((hints & StatusBarManager.NAVIGATION_HINT_IME_SHOWN) != 0);
         if (getImeSwitchButton() != null)
             getImeSwitchButton().setVisibility(showImeButton ? View.VISIBLE : View.GONE);
+            mIsImeButtonVisible = showImeButton;
 
         // Update menu button in case the IME state has changed.
         setMenuVisibility(mShowMenu, true);
@@ -759,7 +761,8 @@ public class NavigationBarView extends LinearLayout {
             return;
         } else if (mMenuVisibility == MENU_VISIBILITY_NEVER) {
             leftMenuKeyView.setVisibility(View.INVISIBLE);
-            rightMenuKeyView.setVisibility(View.INVISIBLE);
+            rightMenuKeyView.setVisibility(
+                    mIsImeButtonVisible ? View.GONE : View.INVISIBLE);
         }
 
 
@@ -773,7 +776,8 @@ public class NavigationBarView extends LinearLayout {
                 && shouldShow;
 
         leftMenuKeyView.setVisibility(showLeftMenuButton ? View.VISIBLE : View.INVISIBLE);
-        rightMenuKeyView.setVisibility(showRightMenuButton ? View.VISIBLE : View.INVISIBLE);
+        rightMenuKeyView.setVisibility(showRightMenuButton ? View.VISIBLE
+                : (mIsImeButtonVisible ? View.GONE : View.INVISIBLE));
         mShowMenu = show;
     }
 
